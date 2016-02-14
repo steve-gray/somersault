8b43ade5684a1a35c2c7788d6da934e9afec79b5
steve-gray <steve@eventualconsistency.net>
2016-02-15 00:43:16 +1100

    Work in progress upload.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 'use strict';
 
-module.exports = require('./lib');
+const somersault = require('./lib');
+
+const container = somersault.createContainer();
+const generator = () => {
+  console.log('Called generator');
+  return {
+    resolved: true
+  };
+};
+
+container.register(['database', 'othertag'], generator);
+container.register(['haterbase', 'othertag'], generator);
+
+const result = container.resolve('database');
+console.log(result);
