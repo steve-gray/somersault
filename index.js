9450534a3d0f057128f4b006d23f0a6be5ebdf38
steve-gray <steve@eventualconsistency.net>
2016-02-15 01:54:28 +1000

    Much more work towards initial version.

--- index.js
+++ index.js
@@ -2,16 +2,4 @@
 
 const somersault = require('./lib');
 
-const container = somersault.createContainer();
-const generator = () => {
-  console.log('Called generator');
-  return {
-    resolved: true
-  };
-};
-
-container.register(['database', 'othertag'], generator);
-container.register(['haterbase', 'othertag'], generator);
-
-const result = container.resolve('database');
-console.log(result);
+module.exports = somersault;
---
+++ tst/registration-build.mocha.js
@@ -0,0 +1,60 @@
+'use strict';
+
+/* global describe, it, before, after, beforeEach, afterEach */
+const chai = require('chai');
+const expect = chai.expect;
+const lib = require('../lib');
+
+describe('Build Tests', () => {
+  class BottomClass {
+    constructor() {
+      this.value = 5;
+    }
+  }
+
+  function returnsValue() {
+    return 3;
+  }
+
+  class MiddleClass {
+    constructor(bottomClass, someFunction) {
+      this.value = bottomClass.value + someFunction;
+      expect(this.value).to.equal(8);
+    }
+  }
+
+  const arrowNoArgs = () => 3;
+
+  function returnsChild(arglessArrow) {
+    expect(arglessArrow).to.equal(3);
+    return arglessArrow + 1;
+  }
+
+  const arrowArgs = (paramFunc) => paramFunc;
+
+  class RootClass {
+    constructor(middleClass, someArrow, paramFunc) {
+      this.middleClass = middleClass;
+      expect(middleClass.value).to.equal(8);
+      this.someArrow = someArrow;
+      expect(someArrow).to.equal(4);
+      this.paramFunc = paramFunc;
+      expect(paramFunc).to.equal(4);
+      this.value = middleClass.value + someArrow + paramFunc;
+      expect(this.value).to.equal(16);
+    }
+  }
+
+  it('Should correctly add up', () => {
+    const container = lib.createContainer();
+    container.register('rootClass', RootClass);         // Complex class
+    container.register('middleClass', MiddleClass);     // Class with mixed params
+    container.register('bottomClass', BottomClass);     // Class, No params
+    container.register('paramFunc', returnsChild);      // Func with params
+    container.register('someFunction', returnsValue);   // Func, no params
+    container.register('someArrow', arrowArgs);         // Arrow with args
+    container.register('arglessArrow', arrowNoArgs);    // Arrow without args
+    const result = container.resolve('rootClass').value;
+    expect(result).to.equal(16);
+  });
+});
